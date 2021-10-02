613eb91ad6f1578d94168eb47f5c1ef4ceab4d5d
crowstorms <88227736+crowstorms@users.noreply.github.com>
2021-10-02 20:49:45 +0800

    Add files via upload

---
+++ debug.c
@@ -0,0 +1,21 @@
+//现在写出修复后的程序
+#include <stdio.h> //是stdio 不是studio
+int main()
+{
+	int n;
+	int n2;
+	int n3;
+	
+	n = 5;
+	n2 = n * n;
+	n3 = n2 * n; //在每个变量和符号间应该空格 
+	
+	
+	printf("n = %d,n squared, = %d,n cubed = %d,\n",n,n2,n3);//n squared是n的平方；n cubed是n的立方 
+	                                                         //\n应写在""之内 
+	return 0;
+	
+	
+ } 
+
+ 
